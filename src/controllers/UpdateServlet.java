5de22dd2583ab32183bcec0bc37ea775cacdfdca
toru.sugiura <toru_sugiura@root-system.net>
2020-08-26 18:04:59 +0900

    edit function and css

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -17,18 +17,26 @@ import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class CreateServlet
+ * Servlet implementation class UpdateServlet
  */
-@WebServlet("/create")
-public class CreateServlet extends HttpServlet {
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateServlet() {
+    public UpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
     /**
@@ -37,12 +45,19 @@ public class CreateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
 
-        String _token = request.getParameter("_token");
+System.out.println( "token = " + (String)request.getParameter("_token"));
+        String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
+System.out.println( "=======>1");
             EntityManager em = DBUtil.createEntityManager();
+System.out.println( "=======>2");
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+//System.out.println( "task id = " +  request.getSession().getAttribute("task_id").toString() );
 
-            Task t = new Task();
-
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+System.out.println( "=======>3");
+            // フォームの内容を各プロパティに上書き
             String title = request.getParameter("title");
             t.setTitle(title);
 
@@ -50,32 +65,35 @@ public class CreateServlet extends HttpServlet {
             t.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            t.setCreated_at(currentTime);
-            t.setUpdated_at(currentTime);
+            t.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
-
-            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
             List<String> errors = TaskValidator.validate(t);
             if(errors.size() > 0) {
+ System.out.println( "=======>4");
                 em.close();
 
                 // フォームに初期値を設定、さらにエラーメッセージを送る
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("message", t);
+                request.setAttribute("task", t);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
                 rd.forward(request, response);
             } else {
+System.out.println( "=======>5");
+                // データベースを更新
                 em.getTransaction().begin();
-                em.persist(t);
                 em.getTransaction().commit();
-                request.getSession().setAttribute("flush", "登録が完了しました。");
+                request.getSession().setAttribute("flush", "更新が完了しました。");
                 em.close();
 
+                // セッションスコープ上の不要になったデータを削除
+                request.getSession().removeAttribute("task_id");
+
+                // indexページへリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
         }
     }
 
